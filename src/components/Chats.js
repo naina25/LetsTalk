89f1962f1765b854bea020cb6a4ba182dd2884a1
naina <nanu02d8@gmail.com>
2021-06-09 23:19:41 +0530

    (Creating Users API) handled the situation of already existing users

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,14 +1,33 @@
-import React, { useCallback } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
 
 import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
 
 const Chats = () => {
   const history = useHistory();
   const { user } = useAuth();
-  console.log(user);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+      return;
+    }
+    axios
+      .get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": "1094b8b7-5125-4b88-b587-450b3cfd49f0",
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      });
+  }, [user, history]);
 
   const handleLogout = async () => {
     await auth.signOut();
